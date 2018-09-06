2a4af581fcf2e838a4033fe95a629194ea8216d7
Matti Dahlbom <matti.dahlbom@qvik.fi>
2018-09-06 13:12:35 +0300

    Added custom font Source Sans Pro

---
+++ NinchatSDK/UI/AuxiliaryViews/VideoCallConsentDialog/NINVideoCallConsentDialog.h
@@ -0,0 +1,13 @@
+//
+//  NINVideoCallConsentDialog.h
+//  NinchatSDK
+//
+//  Created by Matti Dahlbom on 05/09/2018.
+//  Copyright © 2018 Somia Reality Oy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NINVideoCallConsentDialog : UIView
+
+@end
---
+++ NinchatSDK/UI/AuxiliaryViews/VideoCallConsentDialog/NINVideoCallConsentDialog.m
@@ -0,0 +1,21 @@
+//
+//  NINVideoCallConsentDialog.m
+//  NinchatSDK
+//
+//  Created by Matti Dahlbom on 05/09/2018.
+//  Copyright © 2018 Somia Reality Oy. All rights reserved.
+//
+
+#import "NINVideoCallConsentDialog.h"
+
+@implementation NINVideoCallConsentDialog
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
