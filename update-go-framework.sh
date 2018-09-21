ffe7932b36f6cb6b71e377dca9107425613c0b8f
Matti Dahlbom <matti.dahlbom@qvik.fi>
2018-09-21 20:48:48 +0300

    Fixes to the go-lib update script

--- update-go-framework.sh
+++ update-go-framework.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Rebuilding Go SDK framework.."
+echo "Will rebuild the low-level framework."
 
 framework="NinchatLowLevelClient.framework"
 
 mygopath="$GOPATH:`pwd`/go-sdk"
-gocodedir="go-sdk/src/github.com/ninchat/ninchat-go/mobile"
+gocodedir="`pwd`/go-sdk/src/github.com/ninchat/ninchat-go/mobile/"
 tmpframework="/tmp/$framework"
 frameworkdir="Frameworks/$framework"
 
@@ -13,7 +13,7 @@ frameworkdir="Frameworks/$framework"
 rm -rf "$tmpframework"
 
 # Check that the go code dir exists
-if [ -d $gocodedir ]
+if [[ ! -d $gocodedir ]]
 then
     echo "Could not find go code dir: $gocodedir"
     exit 1
@@ -29,6 +29,6 @@ fi
 
 # Copy the main header + the binary over to the framework dir
 cp "$tmpframework/Headers/NINLowLevelClient.objc.h" "$frameworkdir/Headers/"
-cp "$tmpframework/NinchatLowLevelClient" "$frameworkdir/"
+cp "$tmpframework/Client" "$frameworkdir/Versions/Current/NinchatLowLevelClient"
 
 echo "Done."
