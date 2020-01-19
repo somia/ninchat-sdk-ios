a032aa4e44ae36eee059377a13de95fbf60fe86a
Hassan Shahbazi <h-shahbazi@hotmail.com>
2020-01-19 13:30:09 +0200

    remove comments

--- NinchatSDK/UI/ViewControllers/ChatViewController/NINChatViewController.m
+++ NinchatSDK/UI/ViewControllers/ChatViewController/NINChatViewController.m
@@ -730,25 +730,6 @@ static NSString* const kTextInputPlaceholderText = @"Enter your message";
     [self.remoteVideoView renderFrame:nil];
     self.remoteVideoTrack = remoteVideoTrack;
     [self.remoteVideoTrack addRenderer:self.remoteVideoView];
-    
-//    AVAudioSessionPortOverride port = AVAudioSessionPortOverrideSpeaker;
-//    [RTCDispatcher dispatchAsyncOnType:RTCDispatcherTypeAudioSession block:^{
-//        RTCAudioSession *session = [RTCAudioSession sharedInstance];
-//        session.useManualAudio = true;
-//        [session lockForConfiguration];
-//
-//        NSError *error = nil;
-//        [session setMode:AVAudioSessionModeVideoChat error:&error];
-//        NSCAssert(error == nil, @"Error");
-//
-//        [session overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker error:&error];
-//        NSCAssert(error == nil, @"Error");
-//
-//        [session setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:&error];
-//        NSCAssert(error == nil, @"Error");
-//
-//        [session unlockForConfiguration];
-//    }];
 }
 
 - (void)webrtcClient:(NINWebRTCClient *)client didChangeConnectionState:(RTCIceConnectionState)newState {
